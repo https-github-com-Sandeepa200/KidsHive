b710afe01517a9bde2edee9718c5d42df4f007a0
Sandeepa <chemilthas@gmail.com>
2023-03-22 22:08:19 +0530

    sample edit

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ session_start();
         <div class="headBanner" id="headBannerSize">
             <h1>Welcome to the KidsHive</h1>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia doloribus optio excepturi nostrum fuga obcaecati iste ex deleniti libero perspiciatis?
+                enter description, sit amet consectetur adipisicing elit. Mollitia doloribus optio excepturi nostrum fuga obcaecati iste ex deleniti libero perspiciatis?
             </p>
             <h3><a href="loginPage.php">Sign Up</a></h3>
         </div>
