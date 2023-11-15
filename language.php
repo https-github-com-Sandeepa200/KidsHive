cd622ce4afc82b6febbb121e3eec449d90f8c77f
paba <pwkarunarathne28@gmail.com>
2023-11-15 13:23:39 +0530

    Added the language translation

--- language.php
+++ language.php
@@ -1,9 +1,9 @@
 <?php
     /*word dictionary for nav bar*/
     $nav_bar = array(
-        'en'=>array('Home', 'Dashboard', 'Art', 'About'),
-        'si'=>array('ගෙදර', 'උපකරණ පුවරුව', 'කලාව', 'ගැන'),
-        'ta'=>array('வீடு', 'டாஷ்போர்டு', 'கலை', 'பற்றி'),
+        'en'=>array('Home', 'Dashboard', 'Art','About'),
+        'si'=>array('මුල් පිටුව', 'උපකරණ පුවරුව', 'කලාව','අප ගැන'),
+        'ta'=>array('முகப்பு பக்கம்', 'டாஷ்போர்டு', 'கலை', 'எங்களை பற்றி'),
     );
 
     $home_heading = array(
@@ -26,7 +26,7 @@
 
     $heading2 = array(
         'en'=>"Education Through Play!",
-        'si'=>"සෙල්ලම් හරහා අධ්‍යාපනය!",
+        'si'=>"ක්‍රීඩාව හරහා අධ්‍යාපනය!",
         'ta'=>"விளையாட்டு மூலம் கல்வி!",
     );
 
@@ -48,22 +48,10 @@
         'ta'=>"புதிய விஷயங்களைக் கற்றுக்கொள்ளவும் ஆராயவும் உதவும் வேடிக்கையான மற்றும் கல்வி சார்ந்த விளையாட்டுகள், செயல்பாடுகள் மற்றும் வீடியோக்களை இங்கே காணலாம்.",
     );
 
-    $card_1 = array(
-        'en' => "Study with kidweb",
-        'si' => "Kidweb සමඟ අධ්‍යයනය කරන්න",
-        'ta' => "Kidweb மூலம் படிக்கவும்",
-    );
-
-    $card_2 = array(
-        'en' => "Play with kidweb",
-        'si' => "Kidweb සමඟ සෙල්ලම් කරන්න",
-        'ta' => "Kidweb விளையாட",
-    );
-
-    $card_3 = array(
-        'en' => "Draw with kidweb",
-        'si' => "Kidweb සමඟ අඳින්න",
-        'ta' => "Kidweb மூலம் வரையவும்",
+    $cards = array(
+        'en' => array ( "Study with Kids Hive","Play with Kids Hive","Draw with Kids Hive"),
+        'si' => array ("Kids Hive සමඟ පාඩම් කරන්න","Kids Hive සමඟ සෙල්ලම් කරන්න","Kids Hive සමඟ චිත්‍ර අඳින්න"),
+        'ta' => array ("Kids Hive உடன் படிக்கவும்","Kids Hive விளையாட","Kids Hive மூலம் கலைகளை வரையவும்")
     );
 
     $heading4 = array(
@@ -73,9 +61,9 @@
     );
 
     $review = array(
-        'en'=>"Kids Hive offers diverse content, including educational games, videos, and interactive activities. I was pleased to see that many activities on the website have educational value. Kids Hive strikes a good balance between fun and learning from math games to language exercises. The interactive nature of the content keeps children engaged while subtly imparting knowledge.",
-        'si'=>"Kids Hive අධ්‍යාපනික ක්‍රීඩා, වීඩියෝ සහ අන්තර්ක්‍රියාකාරී ක්‍රියාකාරකම් ඇතුළු විවිධ අන්තර්ගතයන් පිරිනමයි. වෙබ් අඩවියේ බොහෝ ක්‍රියාකාරකම් අධ්‍යාපනික වටිනාකමක් ඇති බව දැකීමෙන් මම සතුටු විය. Kids Hive ගණිත ක්‍රීඩාවල සිට භාෂා අභ්‍යාස දක්වා විනෝදය සහ ඉගෙනීම අතර හොඳ සමතුලිතතාවයක් ඇති කරයි. අන්තර්ගතයේ අන්තර්ක්‍රියාකාරී ස්වභාවය, සූක්ෂ්ම ලෙස දැනුම ලබා දෙන අතරම, දරුවන් සම්බන්ධ කර ගනී.",
-        'ta'=>"Kids Hive கல்வி விளையாட்டுகள், வீடியோக்கள் மற்றும் ஊடாடும் நடவடிக்கைகள் உட்பட பல்வேறு உள்ளடக்கத்தை வழங்குகிறது. இணையதளத்தில் பல செயல்பாடுகள் கல்வி மதிப்பைக் கொண்டிருப்பதைக் கண்டு நான் மகிழ்ச்சியடைந்தேன். Kids Hive கணித விளையாட்டுகள் முதல் மொழிப் பயிற்சிகள் வரை வேடிக்கை மற்றும் கற்றல் ஆகியவற்றுக்கு இடையே நல்ல சமநிலையை ஏற்படுத்துகிறது. உள்ளடக்கத்தின் ஊடாடும் தன்மை, நுணுக்கமாக அறிவை வழங்கும்போது குழந்தைகளை ஈடுபாட்டுடன் வைக்கிறது.",
+        'en'=>"Kids Hive offers diverse content, including educational games, videos, and interactive activities. Kids Hive strikes a good balance between fun and learning from math games to language exercises. The interactive nature of the content keeps children engaged while subtly imparting knowledge.",
+        'si'=>"Kids Hive අධ්‍යාපනික ක්‍රීඩා, වීඩියෝ සහ අන්තර්ක්‍රියාකාරී ක්‍රියාකාරකම් ඇතුළු විවිධ අන්තර්ගතයන් පිරිනමයි. Kids Hive ගණිත ක්‍රීඩාවල සිට භාෂා අභ්‍යාස දක්වා විනෝදය සහ ඉගෙනීම අතර හොඳ සමතුලිතතාවයක් ඇති කරයි. අන්තර්ගතයේ අන්තර්ක්‍රියාකාරී ස්වභාවය, සූක්ෂ්ම ලෙස දැනුම ලබා දෙන අතරම, දරුවන් සම්බන්ධ කර ගනී.",
+        'ta'=>"கிட்ஸ் ஹைவ் கல்வி விளையாட்டுகள், வீடியோக்கள் மற்றும் ஊடாடும் நடவடிக்கைகள் உட்பட பல்வேறு உள்ளடக்கத்தை வழங்குகிறது. கிட்ஸ் ஹைவ் கணித விளையாட்டுகள் முதல் மொழிப் பயிற்சிகள் வரை வேடிக்கை மற்றும் கற்றல் ஆகியவற்றுக்கு இடையே நல்ல சமநிலையை ஏற்படுத்துகிறது. உள்ளடக்கத்தின் ஊடாடும் தன்மை, நுணுக்கமாக அறிவை வழங்கும்போது குழந்தைகளை ஈடுபாட்டுடன் வைக்கிறது.",
     );
 
     $name = array(
@@ -97,17 +85,99 @@
     );
 
     $profile_card = array(
-        'en'=>array('Home', 'Dashboard', 'Art', 'About'),
-        'si'=>array('ගෙදර', 'උපකරණ පුවරුව', 'කලාව', 'ගැන'),
-        'ta'=>array('வீடு', 'டாஷ்போர்டு', 'கலை', 'பற்றி'),
+        'en'=>array('Dhanushka', 'Paba', 'Sandeepa', 'Thimali','Software Engineer(UG)'),
+        'si'=>array('ධනුෂ්ක', 'පබා', 'සංදීප', 'තිමාලි','මෘදුකාංග ඉංජිනේරු'),
+        'ta'=>array('தனுஷ்க', 'பபா', 'சந்தீப', 'திமாலி', 'மென்பொருள் பொறியாளர்'),
     );
 
+    $about_btn = array(
+        'en'=>"About Us",
+        'si'=>"අප ගැන",
+        'ta'=>"நம்மைப் பற்றி",
+    );
      
+    $footer_description = array(
+        'en'=>"The Kids Hive website provides for kids below 8 years old and includes incorporating educational content, interactive activities, and age-appropriate design elements to enhance their learning experience while having fun.",
+        'si'=>"Kids Hive වෙබ් අඩවිය වයස අවුරුදු 8 ට අඩු ළමුන් සඳහා සපයන අතර විනෝද වන අතරම ඔවුන්ගේ ඉගෙනීමේ අත්දැකීම් වැඩිදියුණු කිරීම සඳහා අධ්‍යාපනික අන්තර්ගතයන්, අන්තර්ක්‍රියාකාරී ක්‍රියාකාරකම් සහ වයසට ගැළපෙන නිර්මාණ අංග ඇතුළත් වේ.",
+        'ta'=>"Kids Hive இணையதளம் 8 வயதுக்குட்பட்ட குழந்தைகளுக்கு வழங்குகிறது, மேலும் கல்வி உள்ளடக்கம், ஊடாடும் செயல்பாடுகள் மற்றும் வயதுக்கு ஏற்ற வடிவமைப்பு கூறுகளை உள்ளடக்கி அவர்களின் கற்றல் அனுபவத்தை மேம்படுத்துகிறது.",
+    );
 
+    $footer_pages = array(
+        'en'=>array('Home', 'Games','Lessons', 'Art', 'About','Pages'),
+        'si'=>array('මුල් පිටුව', 'අධ්‍යාපනික ක්‍රීඩා','පාඩම්','කලාව', 'අප ගැන','පිටු'),
+        'ta'=>array('முகப்பு பக்கம்', 'கல்வி விளையாட்டுகள்','பாடங்கள்','கலை', 'எங்களை பற்றி', 'பக்கங்கள்'),
+    );
 
+    $programmes = array(
+        'en'=>array('Pre School', 'Grade 1','Grade 2', 'Grade 3','Games'),
+        'si'=>array('පෙර පාසල', '1 ශ්‍රේණිය','2 ශ්‍රේණිය', '3 ශ්‍රේණිය','ක්‍රීඩා'),
+        'ta'=>array('முன் பள்ளி', 'தரம் 1','தரம் 2', 'தரம் 3', 'விளையாட்டுகள்'),
+    );
 
+    $lessons = array(
+        'en'=>array('Math', 'English','Science','Lessons'),
+        'si'=>array('ගණිතය', 'ඉංග්‍රීසි','විද්‍යාව', 'පාඩම්'),
+        'ta'=>array('கணிதம்', 'ஆங்கிலம்','விஞ்ஞானம்', 'பாடங்கள்'),
+    );
 
+    $footer_contact = array(
+        'en'=>array('Contact', 'Privacy Policy - Kids Hive © 2022 - All Rights Reserved'),
+        'si'=>array('අමතන්න', 'රහස්‍යතා ප්‍රතිපත්තිය - Kids Hive © 2022 - සියලුම හිමිකම් ඇවිරිණි'),
+        'ta'=>array('தொடர்பு கொள்ளவும்', 'தனியுரிமைக் கொள்கை - Kids Hive © 2022 - அனைத்து உரிமைகளும் பாதுகாக்கப்பட்டவை'),
+    );
 
+    $about_heading1 = array(
+        'en'=>"About Our Team",
+        'si'=>"අපේ කණ්ඩායම ගැන",
+        'ta'=>"எங்கள் குழு பற்றி",
+    );
 
+    $about_description1 = array(
+        'en'=>"The Kids Hive web application is developed by the Data Pirates Team, a software engineering undergraduates of the Open University Sri Lanka with a focus on educational technology. 
+        Data Pirates has a team of developers, designers, and has who are committed to creating engaging and effective educational software for children.",
+        'si'=>"Kids Hive වෙබ් යෙදුම නිපදවා ඇත්තේ අධ්‍යාපනික තාක්ෂණය කෙරෙහි අවධානය යොමු කරමින් ශ්‍රී ලංකා විවෘත විශ්වවිද්‍යාලයේ මෘදුකාංග ඉංජිනේරු උපාධි අපේක්ෂකයින් වන Data Pirates Team විසිනි. Data Pirates සතුව සංවර්ධකයින්, නිර්මාණකරුවන් කණ්ඩායමක් සිටින අතර ළමුන් සඳහා ආකර්ෂණීය සහ ඵලදායී අධ්‍යාපනික මෘදුකාංගයක් නිර්මාණය කිරීමට කැපවී සිටින අය සිටිති.",
+        'ta'=>"Kids Hive இணையப் பயன்பாடு இலங்கை திறந்த பல்கலைக்கழகத்தின் மென்பொருள் பொறியியல் பட்டதாரிகளான Data Pirates குழுவால் கல்வி தொழில்நுட்பத்தை மையமாகக் கொண்டு உருவாக்கப்பட்டுள்ளது. Data Pirates ஆனது டெவலப்பர்கள், வடிவமைப்பாளர்கள் மற்றும் குழந்தைகளுக்கான ஈடுபாடும் பயனுள்ள கல்வி மென்பொருளை உருவாக்குவதில் உறுதிபூண்டுள்ள குழுவைக் கொண்டுள்ளது.",
+    );
 
-?>
+    $about_heading2 = array(
+        'en'=>"Team Members",
+        'si'=>"කණ්ඩායම් සාමාජිකයින්",
+        'ta'=>"குழு உறுப்பினர்கள்",
+    );
+
+    $about_members = array(
+        'en'=>array('Ms. Paba Waruni','Mr. Dhanushka Dharmasiri', 'Ms. Thimali Ruwnathika','Mr. Sandeepa Rambukwella'),
+        'si'=>array('පබා වරුණි මෙනවිය','ධනුෂ්ක ධර්මසිරි මහතා', 'තිමාලි රුවන්තිකා මෙනවිය', 'සංදීප රඹුක්වැල්ල මහතා ', ),
+        'ta'=>array('மிஸ் பபா வருணி', 'திரு.தனுஷ்க தர்மசிறி', 'மிஸ் திமாலி ருவந்திகா', 'திரு.சந்தீப ரம்புக்வெல்ல'),
+    );
+
+    $art_heading = array(
+        'en'=>array('Draw anything you like','Sorry your browser doesnt support canvas'),
+        'si'=>array('ඔබ කැමති ඕනෑම දෙයක් අඳින්න','සමාවන්න ඔබගේ බ්‍රවුසරය කැන්වසයට සහය නොදක්වයි'),
+        'ta'=>array('நீங்கள் விரும்பும் எதையும் வரையவும்','மன்னிக்கவும், உங்கள் உலாவி கேன்வாஸை ஆதரிக்கவில்லை'),
+    );
+
+    $art_btn = array(
+        'en'=>array('Colour','Brush Size', 'Eraser','Download'),
+        'si'=>array('වර්ණ','බුරුසුවේ ප්‍රමාණය', 'මකනය', 'බාගත කිරීම ', ),
+        'ta'=>array('நிறம்', 'தூரிகை அளவு', 'அழிப்பான்', 'பதிவிறக்குகிறது'),
+    );
+
+    $lesson_heading = array(
+        'en'=>"select what you want to learn",
+        'si'=>"ඔබට ඉගෙනීමට අවශ්‍ය දේ තෝරන්න",
+        'ta'=>"நீங்கள் கற்க விரும்புவதைத் தேர்ந்தெடுக்கவும்",
+    );
+
+    $gameSelecction_heading = array(
+        'en'=>"select your category",
+        'si'=>"ඔබේ කාණ්ඩය තෝරන්න",
+        'ta'=>"உங்கள் வகையைத் தேர்வுசெய்க",
+    );
+
+    $games_headings = array(
+        'en'=>array('Pre School Games','Grade 1 Games', 'Grade 2 Games','Grade 3 Games'),
+        'si'=>array('පෙර පාසල් ක්‍රීඩා','1 ශ්‍රේණියේ ක්‍රීඩා', '2 ශ්‍රේණියේ ක්‍රීඩා', '3 ශ්‍රේණියේ ක්‍රීඩා', ),
+        'ta'=>array('பள்ளிக்கு முந்தைய விளையாட்டுகள்', 'கிரேடு 1 விளையாட்டுகள்', 'கிரேடு 2 விளையாட்டுகள்', 'கிரேடு 3 விளையாட்டுகள்'),
+    );
+?> 
