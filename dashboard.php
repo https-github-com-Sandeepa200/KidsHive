d0d6f27a0b3e6aed8434d7202bd78bafd4f9985b
Sandeepa <chemilthas@gmail.com>
2023-12-02 22:52:27 +0530

    bug fix

--- dashboard.php
+++ dashboard.php
@@ -26,10 +26,8 @@ if ($user_data == "") {
     <link rel="stylesheet" href="css/dashboard.css">
 
     <!--css.animation CDN link-->
-
-    <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
